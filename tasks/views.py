4bec25227fe5b694d0496e544eab8ede179583d5
dmytrorevak <dmytro.revak@gmail.com>
2017-07-27 21:23:24 +0300

    add login template and show all user task on the user-page

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import *
 
 
+@login_required
 def user_page_view(request):
-    return render(request, 'user-page.html')
+    user = request.user
+    tasks = Task.objects.filter(user_id=user.id)
+    return render(request, 'user-page.html', {'tasks': tasks})
--- tasks_list/settings.py
+++ tasks_list/settings.py
@@ -105,6 +105,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tasks_list/static/')
 ]
 
+LOGIN_REDIRECT_URL = '/'
+
 
 try:
     from .local_settings import *
