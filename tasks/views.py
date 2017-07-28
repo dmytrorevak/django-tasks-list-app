a3bbd90418f3e9d71b8dfb1985bd697b1233c7a4
dmytrorevak <dmytro.revak@gmail.com>
2017-07-28 14:00:36 +0300

    add custom sign in form and implement registration logic

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 from .models import *
 
 
@@ -8,3 +9,14 @@ def user_page_view(request):
     user = request.user
     tasks = Task.objects.filter(user_id=user.id)
     return render(request, 'user-page.html', {'tasks': tasks})
+
+
+def sign_up_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/', permanent=True)
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/sign-up.html', {'form': form})
--- tasks_list/urls.py
+++ tasks_list/urls.py
@@ -20,5 +20,6 @@ from tasks import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^$', views.user_page_view, name="user_page"),
+    url(r'^accounts/registration', views.sign_up_view, name='registration'),
+    url(r'^$', views.user_page_view, name='user_page'),
 ]
