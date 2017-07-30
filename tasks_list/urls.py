baf92d6a8039703b3b9e7184fa15b1c22d7ec8a3
dmytrorevak <dmytro.revak@gmail.com>
2017-07-30 20:45:55 +0300

    add custom styles for sign in template

--- tasks_list/urls.py
+++ tasks_list/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.user_page_view, name='user_page'),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^accounts/registration', views.sign_up_view, name='registration'),
+    url(r'^accounts/sign_up', views.sign_up_view, name='sign_up'),
     url(r'^add_task/$', views.add_task_view, name='add_task'),
 ]
